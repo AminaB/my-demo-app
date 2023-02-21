cd37a92b79143f813b0b6074e71a0ba88140b353
AminaB <aminatou.barry97@gmail.com>
2023-02-21 10:37:18 +0100

    String interpolation and property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
+  allowNewServer=false;
+  serverId=10;
+  constructor(){
+    setTimeout(()=>this.allowNewServer=true,2000)
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
